7f9832a40ebb78131c4496f9f862bed538d5cdd2
Felipe Zanfolim da Silva <felipezanfolim@sf.intra>
2018-03-22 16:45:09 -0300

    status online

--- js/scripts.js
+++ js/scripts.js
@@ -72,6 +72,21 @@ app.controller('myCtrl', function($scope,$http) {
 });
 
 
+app.run(function($window, $rootScope) {
+    $rootScope.online = navigator.onLine;
+    $window.addEventListener("offline", function () {
+      $rootScope.$apply(function() {
+        $rootScope.online = false;
+      });
+    }, false);
+    $window.addEventListener("online", function () {
+      $rootScope.$apply(function() {
+        $rootScope.online = true;
+      });
+    }, false);
+});
+
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register('./service-worker.js')
