69731269b66f0f2dcb2d9a424fd3cffdb48cc8b9
Felipe Zanfolim da Silva <felipezanfolim@sf.intra>
2018-03-22 15:11:51 -0300

    inicio gsf boletos

---
+++ js/scripts.js
@@ -0,0 +1,84 @@
+
+
+
+var app = angular.module('myApp', ['angular-loading-bar']);
+
+app.controller('myCtrl', function($scope,$http) {
+    $scope.cpfBenAtual = "";
+    $scope.boletos      = [];
+ 
+
+    $scope.buscaBoleto = function(){
+
+
+        $scope.boletos      = [];
+
+
+
+        // $http.get("http://oss.saofrancisco.com.br/BoletoSAUDE/Boleto?acao=listar&cpf=02168552843")
+        //   .then(function(response) {
+        //       console.log(response.data);
+
+        //       angular.forEach(response.data.boletos, function(value, key) {
+        //         value.nossoNumero   =   value.nossoNumero.slice(-8);
+        //         $scope.boletos.push(value);
+                
+                
+        //       });
+
+
+        //       $http.get("http://oss.saofrancisco.com.br/BoletoSAUDE/Boleto?acao=listar&cpf=" + $scope.cpfBenAtual)
+        //         .then(function(response) {
+
+        //           if(response.data.boletos.length > 0){
+
+        //             angular.forEach(response.data.boletos, function(value, key) {
+        //               value.nossoNumero   =   value.nossoNumero.slice(-8);
+        //               $scope.boletos.push(value);
+        //             });
+        //          }
+                
+        //          //$scope.cpfBenAtual = "";
+
+        //       });
+
+
+        //       // alert($scope.boletos);
+        //   });
+        
+
+          $http.get("http://oss.saofrancisco.com.br/BoletoSAUDE/Boleto?acao=listar&cpf=" + $scope.cpfBenAtual)
+          .then(function(response) {
+
+            if(response.data.boletos.length > 0){
+
+              angular.forEach(response.data.boletos, function(value, key) {
+                value.nossoNumero   =   value.nossoNumero.slice(-8);
+                $scope.boletos.push(value);
+              });
+           }
+          
+           //$scope.cpfBenAtual = "";
+
+        });
+
+
+
+
+
+    }
+
+
+});
+
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('./service-worker.js')
+    .then(function(reg) {
+        console.log('Service worker Registered');
+    })
+    .catch(function (err) {
+        console.log('erro', err);
+    });
+}
---
+++ service-worker.js
@@ -0,0 +1,47 @@
+let cacheName = 'sf-boleto-v.1.0.1';
+let filesToCache = [
+    './',
+    'index.html',
+    'css/colors.css',
+    'css/styles.css',
+    'js/array.observe.polyfill.js',
+    'js/object.observe.polyfill.js',
+    'js/scripts.js',
+    'js/angular/angular.min.js',
+    'js/angular/loading-bar.min.js'
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[ServiceWorker] Installer');
+
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Criando Cache!');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function(key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Remover Cache velho!', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+//carrega os arquivos de cache offline 
+self.addEventListener('fetch', function (e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
+    );
+});
